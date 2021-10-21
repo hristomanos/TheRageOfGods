7f4c27593c4800d91cd5393e2731c5a7b4dd53c0
Christomanos123 <hristoma@gmail.com>
2021-10-21 14:29:11 +0100

    - Added an impact VFX - Added DealDamage script which instantiates the VFX and destoys the fireball gameobject

--- 3DGameAssignment1/Assets/DealDamage.cs
+++ 3DGameAssignment1/Assets/DealDamage.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class DealDamage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   [SerializeField] GameObject impactVFX;
 
-    // Update is called once per frame
-    void Update()
+    bool collided = false;
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.tag != "Bullet" && collision.gameObject.tag != "Player" && !collided)
+        {
+            collided = true;
+
+            GameObject impact = Instantiate(impactVFX, collision.contacts[0].point, Quaternion.identity);
+
+            Destroy(impact, 2);
+            Destroy(gameObject);
+        }
     }
+    
+
+
+
+
+
+
 }
